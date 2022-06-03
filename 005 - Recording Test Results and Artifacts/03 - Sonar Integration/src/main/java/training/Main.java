aa9a5e62fd7f6e17d681c7ca52daa0a76888a98e
Fábio Ferreira <fmmf@live.com.pt>
2022-06-03 16:49:27 +0100

    Update Main.java

--- 005 - Recording Test Results and Artifacts/03 - Sonar Integration/src/main/java/training/Main.java
+++ 005 - Recording Test Results and Artifacts/03 - Sonar Integration/src/main/java/training/Main.java
@@ -1,8 +1,10 @@
 package training;
+import org.apache.log4j.Logger;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.err(new HelloWorld().getText());
+		//System.err(new HelloWorld().getText());
+		LOG.info(new HelloWorld().getText());
 	}
 }
